c18f22afff5009423bdcf77fc485579484a87ec3
Saoki11111 <t.aoki11111@gmail.com>
2019-07-23 17:35:14 +0900

    Update add array sum

--- marubatu.rb
+++ marubatu.rb
@@ -1,16 +1,20 @@
-# 012345678 を並べて○ ☓ ゲームを実装する
+# ○ ☓ ゲームを実装する
+#
+# 座標
+#
+# 0 | 1 | 2
+# 3 | 4 | 5
+# 6 | 7 | 8 
 
 # 入力した数値を配列の該当する場所に格納する
 # 00 -> 0
 # 01 -> 1
 
-# 0 1 2
-# 3 4 5
-# 6 7 8
-
 puts '00|01|02'
 puts '10|11|12'
 puts '20|21|22'
+
+# 配列を準備
 
 array = [
   [0,1,2],
@@ -18,65 +22,49 @@ array = [
   [6,7,8]
 ]
 
+# 入力した文字列を座標の配列に格納する処理 
 i = 0
-answer = 0
+input = 0
 #while i <= 8
 while i < 3
   puts 'A) ○ を置く場所の数を入力してください'
-  answer = gets.chomp
+  input = gets.chomp
 
-  case answer
+  case input 
 
   when "00"
     array[0][0] = 1
-    p array[0]
-    p array[1]
-    p array[2]
 
   when "01"
-    array[0][1] = 2
-    p array[0]
-    p array[1]
-    p array[2]
+    array[0][1] = 1
+
+  when "02"
+    array[0][2] = 1
 
   else
-    p '数値を入力してください'
+    p '正しい数値を入力してください'
     break
   end
 
-# when
-#    array[0][1]
-#    array[0][2]
+
+#  # Bの処理
+#  puts 'B) ☓ を置く場所の数を入力してください'
 #
-#    array[1][0]
-#    array[1][1]
-#    array[1][2]
-#
-#    array[1][0]
-#    array[1][1]
-#    array[1][2]
 
-  puts 'B) ☓ を置く場所の数を入力してください'
-  answer = gets.chomp
-
-  case answer
-
-  when "00"
-    array[0][0] = '☓ '
-    p array[0]
-    p array[1]
-    p array[2]
-
-  when "01"
-    array[0][1] = '☓ '
-    p array[0]
-    p array[1]
-    p array[2]
-
-  else
-    p '数値を入力してください'
-    break
-  end
+  # ループ処理、合計
   i += 1
+  p array[0].inject(:+)
 end
+
+## 配列内の合計から勝敗を判定する処理
+#case array
+#when array[0] == [3]
+#  p "WIN"
+##when array[3,4,5] == 3
+##  p "WIN"
+##when array[6,7,8] == 3
+##  p "WIN"
+#else
+#  p "default"
+#end
 
