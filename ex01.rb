40b782bc6660e68614b8ecd6159e008228f24220
Saoki11111 <t.aoki11111@gmail.com>
2019-08-20 02:52:43 +0900

    Update サンプルコード

--- ex01.rb
+++ ex01.rb
@@ -1,7 +1,28 @@
-require 'scanf'
+#require 'scanf'
+#
+#puts 'a,bを入力してください'
+#a, b = scanf '%d %d'
+#
+#printf "a = %d b = %d\n",a, b
+#printf "a + b = %d\n",a + b
 
-puts 'a,bを入力してください'
-a, b = scanf '%d %d'
+#i = 0
+#while i < 5
+#  if i == 3
+#    print "iは#{i}です。"
+#    break
+#  end
+#  i += 1
+#end
 
-printf "a = %d b = %d\n",a, b
-printf "a + b = %d\n",a + b
+5.times do |i|
+  ans = 2
+  if i == 2
+    puts "iは#{i}です。"
+    if ans == 2
+      puts "ansは#{ans}です。"
+      break
+    end
+  end
+end
+
