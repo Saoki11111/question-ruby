a063a04aea45c5c63c818dd39a411bf2247f22f4
Saoki11111 <t.aoki11111@gmail.com>
2019-09-29 14:04:38 +0900

    Update 処理の分割

--- marubatu.rb
+++ marubatu.rb
@@ -4,31 +4,10 @@
 # TODO
 
 require './calc_marubatu.rb'
-
-def screen(array)
-  array.each do |row|
-    count = 0
-    row.each do |num|
-      case num
-      when 1
-        print('o')
-      when -1
-        print('x')
-      when 0
-        print(' ')
-      end
-      count += 1
-      print('|') if count <= 2
-    end
-    print("\n")
-  end
-end
-
-def set
-  input = gets.chomp
-end
+require './screen_marubatu.rb'
 
 def set_coordinate(array, player)
+  set = gets.chomp
   row, col = set.split('').map(&:to_i)
   array[row][col] = player
 end
@@ -38,14 +17,12 @@ def input_ox(array)
   while i <= 9
     if i.odd?
       puts 'A) ○ を置く場所の数( ex) 01 , 11...)を入力してください'
-      set
       set_coordinate(array, 1)
     else
       puts 'B) ○ を置く場所の数( ex) 01 , 11...)を入力してください'
-      set
       set_coordinate(array, -1)
     end
-    screen(array)
+    Screen.screen(array)
     Calc.calc(array)
     i += 1
     p 'draw' if i == 10
@@ -53,7 +30,9 @@ def input_ox(array)
 end
 
 array = [
-  [0, 0, 0], [0, 0, 0], [0, 0, 0]
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0]
 ]
 
 input_ox(array)
